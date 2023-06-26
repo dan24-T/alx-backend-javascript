e12418fa72ef741af38e3b54a939d302b255d007
dan24-T <106978779+dan24-T@users.noreply.github.com>
2023-06-26 22:21:21 +0300

    Update 100-createIteratorObject.js

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,6 +1,8 @@
 export default function createIteratorObject(report) {
   return (function* _() {
+    //disable-next-line
     for (const department of Object.values(report.allEmployees)) {
+      //disable-next-line
       for (const employee of department) {
         yield employee;
       }
