3a11a3edc5849d1647f4f1a1859824fe8a5d52bf
dan24-T <106978779+dan24-T@users.noreply.github.com>
2023-06-26 21:54:33 +0300

    Update 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+    const task = true;//comment 1
+    const task2 = false;//comment 2
   }
 
   return [task, task2];
