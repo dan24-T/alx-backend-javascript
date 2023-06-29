d4740d236f3efd053419884e10452db10182bad3
dan24-T <106978779+dan24-T@users.noreply.github.com>
2023-06-30 00:06:49 +0300

    Update 100-evcar.js

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,11 +1,1 @@
-import Car from './10-car';
 
-export default class EVCar extends Car {
-  constructor(brand, motor, color, range) {
-    super(brand, motor, color);
-    this._range = range;
-  }
-
-  static get [Symbol.species]() {
-    return Car;
-  }
