ab6226a89f7a4470e9a98ca393dff0afe5903fe3
dan24-T <106978779+dan24-T@users.noreply.github.com>
2023-06-29 23:58:04 +0300

    Update 10-car.js

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,41 +1,15 @@
 export default class Car {
   constructor(brand, motor, color) {
-    this.brand = brand;
-    this.motor = motor;
-    this.color = color;
-  }
-
-  get brand() {
-    return this._brand;
-  }
-
-  set brand(value) {
-    this._brand = value;
-  }
-
-  get motor() {
-    return this._motor;
-  }
-
-  set motor(value) {
-    this._motor = value;
-  }
-
-  get color() {
-    return this._color;
-  }
-
-  set color(value) {
-    this._color = value;
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
   }
 
   static get [Symbol.species]() {
-    return this;
+    return this.prototype.constructor;
   }
 
   cloneCar() {
-    const Species = this.constructor[Symbol.species];
-
-    return new Species();
+    return new (this.constructor[Symbol.species])();
   }
 }
